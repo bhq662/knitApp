41326f5ce3d43ab3e3e12453876d3c38c2bddcc3
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-11-09 18:55:48 +0200

    Added basic CRUD-endpoints

--- knitapp/src/main/java/hh/harjoitustyo/knitapp/web/ProjectsRestController.java
+++ knitapp/src/main/java/hh/harjoitustyo/knitapp/web/ProjectsRestController.java
@@ -1,5 +1,7 @@
 package hh.harjoitustyo.knitapp.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.*;
 import hh.harjoitustyo.knitapp.domain.ProjectRepository;
 import hh.harjoitustyo.knitapp.domain.Project;
@@ -23,5 +25,26 @@ public class ProjectsRestController {
     @GetMapping("/{id}")
     public Optional<Project> one(@PathVariable Long id) {
         return repository.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project create(@RequestBody Project project) {
+        return repository.save(project);
+    }
+
+    @PutMapping("/{id}")
+    public Project update(@PathVariable Long id, @RequestBody Project project) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        project.setProjectId(id);
+        return repository.save(project);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
